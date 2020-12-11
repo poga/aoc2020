25e832bcea55a4db566528ce594fee396c837dbf
Poga Po <poga.po@gmail.com>
2020-12-11 17:08:16 +0800

    format

--- 11/t.js
+++ 11/t.js
@@ -9,11 +9,11 @@ function main (data) {
   //p2(data)
 }
 
-function _get(dp, i) {
+function _get (dp, i) {
   return dp[i] ? dp[i] : 0
 }
 
-function p1(data) {
+function p1 (data) {
   pdata(data)
   p('---')
   let prev = data
@@ -24,15 +24,15 @@ function p1(data) {
   }
   pdata(n)
   occupied = 0
-  for (let i=0; i<n.length; i++) {
-    for (let j=0; j<n[i].length; j++) {
+  for (let i = 0; i < n.length; i++) {
+    for (let j = 0; j < n[i].length; j++) {
       if (n[i][j] === '#') occupied++
     }
   }
   p(occupied)
 }
 
-function p2(data) {
+function p2 (data) {
   pdata(data)
   p('---')
   let prev = data
@@ -43,21 +43,21 @@ function p2(data) {
   }
   pdata(n)
   occupied = 0
-  for (let i=0; i<n.length; i++) {
-    for (let j=0; j<n[i].length; j++) {
+  for (let i = 0; i < n.length; i++) {
+    for (let j = 0; j < n[i].length; j++) {
       if (n[i][j] === '#') occupied++
     }
   }
   p(occupied)
 }
 
-function adj(data, i, j) {
-  return [data[i-1][j-1], data[i][j-1], data[i+1][j-1], data[i-1][j], data[i+1][j], data[i-1][j+1], data[i][j+1], data[i+1][j+1]]
+function adj (data, i, j) {
+  return [data[i - 1][j - 1], data[i][j - 1], data[i + 1][j - 1], data[i - 1][j], data[i + 1][j], data[i - 1][j + 1], data[i][j + 1], data[i + 1][j + 1]]
 }
 
-function los(data, i, j,  i_off, j_off) {
-  let i_n = i+i_off
-  let j_n = j+j_off
+function los (data, i, j, i_off, j_off) {
+  let i_n = i + i_off
+  let j_n = j + j_off
   while (true) {
     if (!data[i_n] || !data[i_n][j_n]) return '.'
     let t = data[i_n][j_n]
@@ -70,7 +70,7 @@ function los(data, i, j,  i_off, j_off) {
   }
 }
 
-function adj_los(data, i, j) {
+function adj_los (data, i, j) {
   let ret = [
     los(data, i, j, -1, -1),
     los(data, i, j, 0, -1),
@@ -85,24 +85,24 @@ function adj_los(data, i, j) {
 }
 
 // remove padding
-function pp(data) {
+function pp (data) {
   let ret = []
-  for (let i=1; i<data.length-1; i++) {
-    ret.push(data[i].slice(1, data[i].length-1))
+  for (let i = 1; i < data.length - 1; i++) {
+    ret.push(data[i].slice(1, data[i].length - 1))
   }
 
   return ret
 }
 
-function pdata(data) {
-  pp(data).forEach( r => p(r.join('')) )
+function pdata (data) {
+  pp(data).forEach(r => p(r.join('')))
 }
 
-function next(data, los_func, max_occupied_adj) {
+function next (data, los_func, max_occupied_adj) {
   let next = []
-  for (let i=0; i<data.length;i++) {
+  for (let i = 0; i < data.length; i++) {
     let row = []
-    for (let j=0; j<data[0].length; j++) {
+    for (let j = 0; j < data[0].length; j++) {
       row.push(data[i][j])
 
     }
@@ -110,13 +110,13 @@ function next(data, los_func, max_occupied_adj) {
   }
 
 
-  for (let i=1; i<data.length-1; i++) {
-    for (let j=1; j<data[i].length; j++) {
+  for (let i = 1; i < data.length - 1; i++) {
+    for (let j = 1; j < data[i].length; j++) {
 
-      if (data[i][j]==='L') {
+      if (data[i][j] === 'L') {
         let occupied = false
-        los_func(data, i,j).forEach(n => {
-          if (n ==='#') {
+        los_func(data, i, j).forEach(n => {
+          if (n === '#') {
             occupied = true
           }
         })
@@ -130,8 +130,8 @@ function next(data, los_func, max_occupied_adj) {
 
       if (data[i][j] === '#') {
         let occupied = 0
-        los_func(data, i,j).forEach(n => {
-          if (n ==='#') {
+        los_func(data, i, j).forEach(n => {
+          if (n === '#') {
             occupied++
           }
         })
@@ -158,12 +158,12 @@ rl.on('line', function (line) {
   // load data with padding
   if (line_len === 0) {
     line_len = line.length
-    data.push('.'.repeat(line_len+2).split(''))
+    data.push('.'.repeat(line_len + 2).split(''))
   }
   data.push(['.', ...line.split(''), '.'])
 })
 
 rl.on('close', () => {
-  data.push('.'.repeat(line_len+2).split(''))
+  data.push('.'.repeat(line_len + 2).split(''))
   main(data)
 })
